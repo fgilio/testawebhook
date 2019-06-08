ebbf5a5be11835d8c04e6e485d56291d4011cf71
Franco Gilio <franco.gilio@gmail.com>
2019-06-08 00:03:25 -0300

    Add Laravel Telescope

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
         App\Providers\AppServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
